d323effb1e986b7bfdce7ccafc07cbb4bb41e421
TarCode <tarcode33@gmail.com>
2018-10-24 13:43:53 +0200

    updated middleware

--- middleware/headers.js
+++ middleware/headers.js
@@ -1,20 +1,26 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import cors from 'cors'
+import path from 'path'
+import session from 'express-session'
+import errorHandler from 'errorhandler'
 
 const app = express()
+const isProduction = process.env.NODE_ENV === 'production';
 
+
+
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'passport-tx', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
+
+if(!isProduction) {
+    app.use(errorHandler());
+}
 
 app.all("/*", function(req, res, next) {
-  // CORS headers
-  res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  // Set custom headers for CORS
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-type,Accept,X-Access-Token,X-Key"
-  );
   if (req.method == "OPTIONS") {
     res.status(200).end();
   } else {
