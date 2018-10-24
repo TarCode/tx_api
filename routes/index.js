4c3c1b5b1b867530c2ec55e5efb007a552fbcbde
TarCode <tarcode33@gmail.com>
2018-10-24 12:17:12 +0200

    updated swagger spec

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
 import express from 'express'
 import mongoose from 'mongoose'
 
-import { getAccounts, createAccount, getTransactions, createTransfer } from '../controllers'
+import { 
+    getAccounts, 
+    createAccount, 
+    getTransactions, 
+    createTransfer 
+} from '../controllers'
 
 const app = express()
 
@@ -12,11 +17,9 @@ mongoose.connect(uri, { replicaSet: 'rs', useNewUrlParser: true  });
 // mongoose.connection.dropDatabase();
 
 app.get('/accounts', getAccounts)
-  
-app.post('/accounts/create', createAccount)
+app.post('/accounts', createAccount)
 
 app.get('/transactions', getTransactions)
-
-app.post('/transfer', createTransfer)
+app.post('/transactions/send', createTransfer)
 
 module.exports = app;
